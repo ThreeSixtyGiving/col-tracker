c9b35333e67eb713ea2830d2aa0eb633907bc66d
David Kane <david@dkane.net>
2022-11-08 12:12:05 +0000

    copy changes

--- coltracker/components/tab_dashboard.py
+++ coltracker/components/tab_dashboard.py
@@ -25,14 +25,14 @@ def tab_dashboard(data, all_data):
                         children=[
                             dcc.Markdown(
                                 """
-        Grants are included if the use the terms "cost of living" somewhere
-        in the grant description, title, or grant programme. This is based on
-        [guidance for grantmakers issued by 360Giving](https://www.threesixtygiving.org/2022/09/28/col/).
+        Grants are included if they use the terms "cost of living" somewhere in the grant 
+        description, title, or grant programme description or title. 
+        This is based on the [guidance for grantmakers issued by 360Giving](https://www.threesixtygiving.org/2022/09/28/col/) on tagging grants.
 
-        This data is based on UK foundations reporting grants using the
-        [360Giving Data Standard](http://standard.threesixtygiving.org/en/latest/). It only includes grants awarded in
-        British Pounds that have already been made (rather than amounts
-        committed to grant programmes).
+        This tracker is possible thanks to UK grantmakers publishing grants data using the 
+        [360Giving Data Standard](http://standard.threesixtygiving.org/en/latest/).
+        It only includes grants awarded in British Pounds (GBP) that have already been made 
+        (rather than amounts committed to grant programmes).
     """
                             )
                         ],
@@ -42,11 +42,11 @@ def tab_dashboard(data, all_data):
                         children=[
                             dcc.Markdown(
                                 """
-        Not all foundations publish their grants as open data, and some
+        Not all grantmakers publish their grants as open data, and some
         publishers do not immediately publish their latest data.
 
         Some of the data includes grants made to other grantmakers to distribute. You can choose to exclude
-        these grants from the analysis to prevent double counting.
+        these grants from the data to prevent skews to analysis this might cause.
     """
                             ),
                             dcc.Markdown(
